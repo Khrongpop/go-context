6120fa07c846d8db55c3c0ed7cf6a231cef23710
Khrongpop Phonngam <mr.khrongpop@gmail.com>
2019-03-08 19:52:57 +0700

    repair code

--- main.go
+++ main.go
@@ -9,31 +9,31 @@ import (
 
 func main() {
 	fmt.Println("start process context with time out...")
-	wg := sync.WaitGroup()
+	wg := sync.WaitGroup{}
 
 	// Create Context with timeout
 	tmo := 3 * time.Second
 	ctxCooking, cancelCooking := context.WithTimeout(context.Background(), tmo)
-	defer cancelCooking
+	defer cancelCooking()
 
 	wg.Add(1)
 	// Split cooking cast
 	go func() {
 		defer wg.Done()
-		cooking(ctx,"Fried egg",10,"c")
+		cooking(ctxCooking,"Fried egg",10,"c")
 	}()
 	wg.Wait()
 }
 
 func cooking(ctx context.Context, food string, m int, chef string) {
-	for i:=1; i<=n; i++ {
+	for i:=1; i<=m; i++ {
 		select {
 			case<-ctx.Done():
-				fmt.Printlf("Chef %v cooked %v total %v dish \n",chef,food,i-1)
+				fmt.Printf("Chef %v cooked %v total %v dish \n",chef,food,i-1)
 				return
 			default: 
-				fmt.Printlf("Chef %v cooking %v : %v \n",chef,food,i)
-				time.Sleep(i*time.Second)
+				fmt.Printf("Chef %v cooking %v : %v \n",chef,food,i)
+				time.Sleep(1*time.Second)
 		}
 	}
 	return
